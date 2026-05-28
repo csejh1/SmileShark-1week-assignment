4db2774005078bb24c4a1b44be745f5ebd0f568a
csejh1 <jerrysong4912@gmail.com>
2026-05-28 10:55:26 +0900

    test: WAS pipeline trigger test

--- spring-petclinic-reactjs/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ spring-petclinic-reactjs/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class PetClinicApplication {
 
 	// 환경변수 CORS_ALLOWED_ORIGIN 이 없으면 localhost:4444 를 기본값으로 사용
+	// 테스트용 주석
 	@Value("${CORS_ALLOWED_ORIGIN:http://localhost:4444}")
 	private String corsAllowedOrigin;
 
@@ -27,8 +28,8 @@ public class PetClinicApplication {
 						.allowedOriginPatterns(
 								"http://localhost:4444",
 								"http://localhost:3000",
-								corsAllowedOrigin   // AWS 배포 시 CloudFront 도메인이 주입됨
-						)
+								corsAllowedOrigin // AWS 배포 시 CloudFront 도메인이 주입됨
+				)
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
 						.allowedHeaders("*")
 						.exposedHeaders("errors", "content-type");
